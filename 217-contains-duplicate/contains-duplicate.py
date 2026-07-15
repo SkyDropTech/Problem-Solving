03b5b244a218ef6ee9677d9e20f4544312f1e9b4
SkyDropTech <rishikeshtechofficial@gmail.com>
2026-07-15 11:06:50 +0530

    Time: 15 ms (47.08%) | Memory: 37.3 MB (6.16%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq={} 
+        for i in nums:
+            if i not in freq:
+                freq[i]=1 
+            else:
+                return True
+        else:
+            return False
