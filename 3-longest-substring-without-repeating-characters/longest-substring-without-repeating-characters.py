f1ad8d2418b94caf8af73bfa0ef49fcb1bb0fbd3
SkyDropTech <rishikeshtechofficial@gmail.com>
2026-08-31 20:19:59 +0530

    Time: 185 ms (50.12%) | Memory: 19.9 MB (42.22%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,11 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        res=""
-        max_val=0
-        for ch in s:
-            while ch in res:
-                res=res[1:] 
-            res+=ch
-            max_val=max(max_val,len(res))
-        return max_val
+        # I probabily used AI in this question because i dont know what i have done or cannot understand the solution
+        # So now lets solve this question using Hash Map or Table with optimized way 
+        mp={} 
+        left=0 
+        ans=0 
+        for right, ch in enumerate(s):
+            if ch in mp:
+                left=max(left,mp[ch]+1) 
+            mp[ch]=right 
+            ans=max(ans,right-left+1)
+        return ans
+        # res=""
+        # max_val=0
+        # for ch in s:
+        #     while ch in res:
+        #         res=res[1:] 
+        #     res+=ch
+        #     max_val=max(max_val,len(res))
+        # return max_val
 
