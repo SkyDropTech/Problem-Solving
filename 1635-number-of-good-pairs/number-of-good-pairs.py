fa4954e719eefad1cad4d9939dfc63bd082a96d1
SkyDropTech <rishikeshtechofficial@gmail.com>
2026-09-09 10:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.98%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,18 +1,11 @@
 class Solution:
-    def numIdenticalPairs(self, nums):
-        #Bad Apporach because it is using 0(n^2) so we will solve using hashmap
-        freq={}
-        ans=0          
-        for x in nums:
-            ans+=freq.get(x,0) 
-            freq[x]=freq.get(x,0)+1 
-        return ans
-
-        # count = 0
-
-        # for i in range(len(nums)):
-        #     for j in range(i + 1, len(nums)):
-        #         if nums[i] == nums[j]:
-        #             count += 1
-
-        # return count
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        freq={} 
+        ct=0
+        for i in nums:
+            if i in freq:
+                ct+=freq[i] 
+                freq[i]+=1
+            else:
+                freq[i]=1
+        return ct
