99850a92070cfe61c860138c7cdf0bf793e7ae0b
SkyDropTech <rishikeshtechofficial@gmail.com>
2026-09-16 23:11:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (77.63%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,6 +1,7 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        nums1[:]=sorted(nums1[:m]+nums2)
-        return nums1            
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        nums1[:]=sorted(nums1[:m]+nums2 )
+        return (nums1)
+
 
         
