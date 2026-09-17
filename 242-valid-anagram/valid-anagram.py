86ed92403194804b06b334f1223ea1ca59976366
SkyDropTech <rishikeshtechofficial@gmail.com>
2026-09-17 22:13:10 +0530

    Time: 19 ms (16.03%) | Memory: 19.8 MB (23.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s="".join(sorted(s)) 
-        t="".join(sorted(t))
-        if s==t:
+        ss="".join(sorted(s))
+        tt="".join(sorted(t))
+        if ss==tt:
             return True 
         return False
